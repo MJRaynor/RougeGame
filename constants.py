31ec1a28ef8c0c064b158795ea5b9b50b9d2e98a
Rey <MagistrateJRaynor@gmail.com>
2018-11-10 22:04:14 -0600

    added FOV

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import pygame
+import libtcodpy as libtcod
 
 pygame.init()
 
@@ -26,7 +27,16 @@ COLOR_DEFAULT_BG = COLOR_GREY
 
 
 #SPRITES
-S_PLAYER = pygame.image.load("data/python.png")
-S_ENEMY  = pygame.image.load("data/crab.png")
-S_WALL   = pygame.image.load("data/wall.jpg")
-S_FLOOR  = pygame.image.load("data/floor.jpg")
+S_PLAYER            = pygame.image.load("data/python.png")
+S_ENEMY             = pygame.image.load("data/crab.png")
+
+S_WALL              = pygame.image.load("data/wall.jpg")
+S_WALLEXPLORED      = pygame.image.load("data/wallunseen.png")
+
+S_FLOOR             = pygame.image.load("data/floor.jpg")
+S_FLOOREXPLORED     = pygame.image.load("data/floorunseen.png")
+
+#FOV SETTINGS
+FOV_ALGO        = libtcod.FOV_BASIC
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS    = 10
